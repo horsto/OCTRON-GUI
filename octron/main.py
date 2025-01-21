08c368c326bd0b35971d1a53f8c1eb0deba7342f
Hotte <hobenhaus@gmail.com>
2025-01-21 12:45:37 -0500

    First small interaction and notebook

--- octron/main.py
+++ octron/main.py
@@ -19,17 +19,34 @@ class octron_widget(QWidget):
     '''
     def __init__(self, viewer: 'napari.viewer.Viewer', parent=None):
         super().__init__(parent)
+        
+        
         self._viewer = viewer
-
+        
         # Create a button
-        btn = QPushButton("Click me")
+        btn = QPushButton("Click me 🐙")
         # Connect the click event to a function
         btn.clicked.connect(self._on_click)
+        btn.setFixedWidth(150)
+
+
+        # Forwards backwards buttons 
+
+        forw_btn = QPushButton("Next")
+        forw_btn.clicked.connect(self._forw_1frame)
+        forw_btn.setFixedWidth(50)
+        
 
 
         self.setLayout(QHBoxLayout())
         # add it to the layout
         self.layout().addWidget(btn)
+        self.layout().addWidget(forw_btn)
+        
 
     def _on_click(self):
-        show_info('Hello, world!')
+        show_info('Hello and welcome to OCTRON!\nOctopuses are amazing creatures 🐙')
+        
+    def _forw_1frame(self):
+        current_indices = self._viewer.dims.current_step
+        self._viewer.dims.set_point(0,current_indices[0]+100)
