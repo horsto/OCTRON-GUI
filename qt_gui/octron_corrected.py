80a87df10bf8af794c90a0b5adc76cce9083b5b5
Hotte <hobenhaus@gmail.com>
2025-02-02 16:31:30 -0500

    Corrected start up GUI code and layout

--- qt_gui/octron_corrected.py
+++ qt_gui/octron_corrected.py
@@ -1,47 +1,19 @@
-'''
-OCTRON
+# -*- coding: utf-8 -*-
 
-
-
-'''
-import os 
-from pathlib import Path
-cur_path = Path(os.path.abspath(__file__)).parent
-
+################################################################################
+## Form generated from reading UI file 'octron.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.16
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
 from qtpy.QtCore import *  # type: ignore
 from qtpy.QtGui import *  # type: ignore
 from qtpy.QtWidgets import *  # type: ignore
 
 
-import napari
-import napari.window
-from napari.utils.notifications import show_info, show_warning
-
-
-# SAM2 specific 
-import os
-# if using Apple MPS, fall back to CPU for unsupported ops
-os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
-import numpy as np
-from octron.sam2_octron.helpers.build_sam2_octron import build_sam2_octron  
-
-
-from qtpy.QtWidgets import QWidget
-
-class octron_widget(QWidget):
-    '''
-    
-    '''
-    def __init__(self, viewer: 'napari.viewer.Viewer', parent=None):
-        super().__init__(parent)
-        
-        
-        self._viewer = viewer
-        
-        self.setupUi()
-        
-        # self.predictor, self.device = self._initialize_sam2()
+class Ui_octron_widgetui(object):
     def setupUi(self):
         if not self.objectName():
             self.setObjectName(u"self")
@@ -152,42 +124,3 @@ class octron_widget(QWidget):
 #endif // QT_CONFIG(tooltip)
     # retranslateUi
 
-
-
-
-     
-     
-     
-     
-    # def _initialize_sam2(self):
-    #     '''
-    #     Initialize the SAM2 model
-    #     '''
-    #     sam2_folder = Path('sam2_octron')
-        
-        
-    #     # TODO: Make checkpoint path and config file path configurable  
-    #     checkpoint = 'sam2.1_hiera_large.pt' # under folder /checkpoints
-    #     model_cfg = 'sam2.1/sam2.1_hiera_l.yaml' # under folder /configs
-    #     # ------------------------------------------------------------------------------------
-    #     sam2_checkpoint = cur_path / sam2_folder / Path(f'checkpoints/{checkpoint}')
-    #     model_cfg = Path(f'configs/{model_cfg}')
-        
-    #     assert sam2_checkpoint.exists(), f'Checkpoint file does not exist: {sam2_checkpoint}'
-    #     assert (cur_path/sam2_folder/model_cfg).exists(), f'Config file does not exist: {cur_path/sam2_folder/model_cfg}'
-    #     predictor, device  = build_sam2_video_predictor_octron(config_file=model_cfg.as_posix(), 
-    #                                                            ckpt_path=sam2_checkpoint.as_posix(), 
-    #                                                            )
-                
-    #     return predictor, device    
-    
-    # def _on_click(self):
-    #     show_info('Hello and welcome to OCTRON!\nOctopuses are amazing creatures 🐙')
-        
-    # def _forw_1frame(self):
-    #     current_indices = self._viewer.dims.current_step
-    #     self._viewer.dims.set_point(0,current_indices[0]+100)
-        
-        
-        
-        
