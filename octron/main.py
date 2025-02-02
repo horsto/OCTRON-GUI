6ab6d38aeaa60ba936cdff5dc78644f296654a4f
Hotte <hobenhaus@gmail.com>
2025-02-02 11:47:06 -0500

    Humble beginnings of a UI design ...
    Added Octron logo and widget designer outputs

--- octron/main.py
+++ octron/main.py
@@ -8,11 +8,15 @@ import os
 from pathlib import Path
 cur_path = Path(os.path.abspath(__file__)).parent
 
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+
+from qtpy.QtCore import *  # type: ignore
+from qtpy.QtGui import *  # type: ignore
+from qtpy.QtWidgets import *  # type: ignore
+
 
 import napari
 import napari.window
-from napari.utils.notifications import show_info
+from napari.utils.notifications import show_info, show_warning
 
 
 # SAM2 specific 
@@ -35,54 +39,144 @@ class octron_widget(QWidget):
         
         self._viewer = viewer
         
-        # Create a button
-        btn = QPushButton("Click me 🐙")
-        # Connect the click event to a function
-        btn.clicked.connect(self._on_click)
-        btn.setFixedWidth(150)
+        self.setupUi()
+        
+        # self.predictor, self.device = self._initialize_sam2()
+        
+  
+    def setupUi(self):
+        if not self.objectName():
+            self.setObjectName(u"self")
+        self.setEnabled(True)
+        #self.resize(388, 655)
+        self.setMinimumSize(QSize(410, 0))
+        self.verticalLayoutWidget = QWidget(self)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(0, 0, 398, 611))
+        self.mainLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.mainLayout.setSpacing(0)
+        self.mainLayout.setObjectName(u"mainLayout")
+        self.mainLayout.setSizeConstraint(QLayout.SizeConstraint.SetMinimumSize)
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.octron_logo = QLabel(self.verticalLayoutWidget)
+        self.octron_logo.setObjectName(u"octron_logo")
+        self.octron_logo.setEnabled(True)
+        self.octron_logo.setPixmap(QPixmap(u"qt_gui/octron_logo.svg"))
+        self.octron_logo.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+
+        self.mainLayout.addWidget(self.octron_logo)
+
+        self.toolBox = QToolBox(self.verticalLayoutWidget)
+        self.toolBox.setObjectName(u"toolBox")
+        self.toolBox.setCursor(QCursor(Qt.ArrowCursor))
+        self.project_tab = QWidget()
+        self.project_tab.setObjectName(u"project_tab")
+        self.project_tab.setGeometry(QRect(0, 0, 396, 334))
+        self.verticalLayoutWidget_2 = QWidget(self.project_tab)
+        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
+        self.verticalLayoutWidget_2.setGeometry(QRect(0, 0, 381, 171))
+        self.project_tab_vertical_layout = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.project_tab_vertical_layout.setObjectName(u"project_tab_vertical_layout")
+        self.project_tab_vertical_layout.setContentsMargins(0, 0, 0, 0)
+        self.create_project_button = QPushButton(self.verticalLayoutWidget_2)
+        self.create_project_button.setObjectName(u"create_project_button")
+        self.create_project_button.setMouseTracking(False)
+        self.create_project_button.setContextMenuPolicy(Qt.ContextMenuPolicy.NoContextMenu)
+        icon = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::ListAdd"))
+        self.create_project_button.setIcon(icon)
+
+        self.project_tab_vertical_layout.addWidget(self.create_project_button, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
+
+        icon1 = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::DocumentOpen"))
+        self.toolBox.addItem(self.project_tab, icon1, u"Project")
+        self.annotate_tab = QWidget()
+        self.annotate_tab.setObjectName(u"annotate_tab")
+        self.annotate_tab.setGeometry(QRect(0, 0, 396, 334))
+        icon2 = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::MediaPlaybackStop"))
+        self.toolBox.addItem(self.annotate_tab, icon2, u"Create training data")
+        self.train_tab = QWidget()
+        self.train_tab.setObjectName(u"train_tab")
+        self.train_tab.setGeometry(QRect(0, 0, 396, 334))
+        icon3 = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::Computer"))
+        self.toolBox.addItem(self.train_tab, icon3, u"Train model")
+        self.predict_tab = QWidget()
+        self.predict_tab.setObjectName(u"predict_tab")
+        self.predict_tab.setGeometry(QRect(0, 0, 396, 334))
+        icon4 = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::ViewFullscreen"))
+        self.toolBox.addItem(self.predict_tab, icon4, u"Predict on new videos")
+
+        self.mainLayout.addWidget(self.toolBox)
 
 
-        # Forwards backwards buttons 
+        self.retranslateUi()
 
-        forw_btn = QPushButton("Next")
-        forw_btn.clicked.connect(self._forw_1frame)
-        forw_btn.setFixedWidth(50)
-        
+        self.toolBox.setCurrentIndex(0)
 
 
-        self.setLayout(QHBoxLayout())
-        # add it to the layout
-        self.layout().addWidget(btn)
-        self.layout().addWidget(forw_btn)
+        QMetaObject.connectSlotsByName(self)
+    # setupUi
+
+    def retranslateUi(self):
+        self.setWindowTitle(QCoreApplication.translate("self", u"octron_gui", None))
+        self.octron_logo.setText("")
+        self.create_project_button.setText(QCoreApplication.translate("self", u"Create Project", None))
+        self.toolBox.setItemText(self.toolBox.indexOf(self.project_tab), QCoreApplication.translate("self", u"Project", None))
+#if QT_CONFIG(tooltip)
+        self.toolBox.setItemToolTip(self.toolBox.indexOf(self.project_tab), QCoreApplication.translate("self", u"Create new octron projects or load existing ones", None))
+#endif // QT_CONFIG(tooltip)
+        self.toolBox.setItemText(self.toolBox.indexOf(self.annotate_tab), QCoreApplication.translate("self", u"Create training data", None))
+#if QT_CONFIG(tooltip)
+        self.toolBox.setItemToolTip(self.toolBox.indexOf(self.annotate_tab), QCoreApplication.translate("self", u"Create segmentation data that can be used to train models ", None))
+#endif // QT_CONFIG(tooltip)
+        self.toolBox.setItemText(self.toolBox.indexOf(self.train_tab), QCoreApplication.translate("self", u"Train model", None))
+#if QT_CONFIG(tooltip)
+        self.toolBox.setItemToolTip(self.toolBox.indexOf(self.train_tab), QCoreApplication.translate("self", u"Train model with generated training data", None))
+#endif // QT_CONFIG(tooltip)
+        self.toolBox.setItemText(self.toolBox.indexOf(self.predict_tab), QCoreApplication.translate("self", u"Predict on new videos", None))
+#if QT_CONFIG(tooltip)
+        self.toolBox.setItemToolTip(self.toolBox.indexOf(self.predict_tab), QCoreApplication.translate("self", u"Use trained models to run predictions on new videos", None))
+#endif // QT_CONFIG(tooltip)
+    # retranslateUi
+
+   
+     
+     
+     
+     
+     
+     
+     
+     
+     
+    # def _initialize_sam2(self):
+    #     '''
+    #     Initialize the SAM2 model
+    #     '''
+    #     sam2_folder = Path('sam2_octron')
         
-        self.predictor, self.device = self._initialize_sam2()
         
+    #     # TODO: Make checkpoint path and config file path configurable  
+    #     checkpoint = 'sam2.1_hiera_large.pt' # under folder /checkpoints
+    #     model_cfg = 'sam2.1/sam2.1_hiera_l.yaml' # under folder /configs
+    #     # ------------------------------------------------------------------------------------
+    #     sam2_checkpoint = cur_path / sam2_folder / Path(f'checkpoints/{checkpoint}')
+    #     model_cfg = Path(f'configs/{model_cfg}')
         
-    def _initialize_sam2(self):
-        '''
-        Initialize the SAM2 model
-        '''
-        sam2_folder = Path('sam2_octron')
-        
-        
-        # TODO: Make checkpoint path and config file path configurable  
-        checkpoint = 'sam2.1_hiera_large.pt' # under folder /checkpoints
-        model_cfg = 'sam2.1/sam2.1_hiera_l.yaml' # under folder /configs
-        # ------------------------------------------------------------------------------------
-        sam2_checkpoint = cur_path / sam2_folder / Path(f'checkpoints/{checkpoint}')
-        model_cfg = Path(f'configs/{model_cfg}')
-        
-        assert sam2_checkpoint.exists(), f'Checkpoint file does not exist: {sam2_checkpoint}'
-        assert (cur_path/sam2_folder/model_cfg).exists(), f'Config file does not exist: {cur_path/sam2_folder/model_cfg}'
-        predictor, device  = build_sam2_video_predictor_octron(config_file=model_cfg.as_posix(), 
-                                                               ckpt_path=sam2_checkpoint.as_posix(), 
-                                                               )
+    #     assert sam2_checkpoint.exists(), f'Checkpoint file does not exist: {sam2_checkpoint}'
+    #     assert (cur_path/sam2_folder/model_cfg).exists(), f'Config file does not exist: {cur_path/sam2_folder/model_cfg}'
+    #     predictor, device  = build_sam2_video_predictor_octron(config_file=model_cfg.as_posix(), 
+    #                                                            ckpt_path=sam2_checkpoint.as_posix(), 
+    #                                                            )
                 
-        return predictor, device    
+    #     return predictor, device    
     
-    def _on_click(self):
-        show_info('Hello and welcome to OCTRON!\nOctopuses are amazing creatures 🐙')
+    # def _on_click(self):
+    #     show_info('Hello and welcome to OCTRON!\nOctopuses are amazing creatures 🐙')
         
-    def _forw_1frame(self):
-        current_indices = self._viewer.dims.current_step
-        self._viewer.dims.set_point(0,current_indices[0]+100)
+    # def _forw_1frame(self):
+    #     current_indices = self._viewer.dims.current_step
+    #     self._viewer.dims.set_point(0,current_indices[0]+100)
+        
+        
+        
+        
--- qt_gui/octron.py
+++ qt_gui/octron.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'octron.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.13
+## Created by: Qt User Interface Compiler version 5.15.16
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -13,60 +13,98 @@ from PySide2.QtGui import *  # type: ignore
 from PySide2.QtWidgets import *  # type: ignore
 
 
-class Ui_octron_gui(object):
-    def setupUi(self, octron_gui):
-        if not octron_gui.objectName():
-            octron_gui.setObjectName(u"octron_gui")
-        octron_gui.setEnabled(True)
-        octron_gui.resize(388, 655)
-        self.tabs = QTabWidget(octron_gui)
-        self.tabs.setObjectName(u"tabs")
-        self.tabs.setGeometry(QRect(10, 10, 371, 591))
-        self.tabs.setTabShape(QTabWidget.TabShape.Rounded)
-        self.tab = QWidget()
-        self.tab.setObjectName(u"tab")
-        self.verticalLayoutWidget = QWidget(self.tab)
+class Ui_octron_widgetui(object):
+    def setupUi(self, octron_widgetui):
+        if not octron_widgetui.objectName():
+            octron_widgetui.setObjectName(u"octron_widgetui")
+        octron_widgetui.setEnabled(True)
+        octron_widgetui.resize(410, 655)
+        octron_widgetui.setMinimumSize(QSize(400, 0))
+        self.verticalLayoutWidget = QWidget(octron_widgetui)
         self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(10, 10, 351, 541))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setSpacing(-1)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.pushButton = QPushButton(self.verticalLayoutWidget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setCheckable(False)
-        self.pushButton.setAutoDefault(False)
+        self.verticalLayoutWidget.setGeometry(QRect(0, 0, 502, 611))
+        self.mainLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.mainLayout.setSpacing(0)
+        self.mainLayout.setObjectName(u"mainLayout")
+        self.mainLayout.setSizeConstraint(QLayout.SizeConstraint.SetMinimumSize)
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.octron_logo = QLabel(self.verticalLayoutWidget)
+        self.octron_logo.setObjectName(u"octron_logo")
+        self.octron_logo.setEnabled(True)
+        self.octron_logo.setPixmap(QPixmap(u"octron_logo.svg"))
+        self.octron_logo.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
 
-        self.horizontalLayout_2.addWidget(self.pushButton, 0, Qt.AlignmentFlag.AlignTop)
+        self.mainLayout.addWidget(self.octron_logo)
+
+        self.toolBox = QToolBox(self.verticalLayoutWidget)
+        self.toolBox.setObjectName(u"toolBox")
+        self.toolBox.setCursor(QCursor(Qt.ArrowCursor))
+        self.project_tab = QWidget()
+        self.project_tab.setObjectName(u"project_tab")
+        self.project_tab.setGeometry(QRect(0, 0, 500, 334))
+        self.verticalLayoutWidget_2 = QWidget(self.project_tab)
+        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
+        self.verticalLayoutWidget_2.setGeometry(QRect(0, 0, 381, 171))
+        self.project_tab_vertical_layout = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.project_tab_vertical_layout.setObjectName(u"project_tab_vertical_layout")
+        self.project_tab_vertical_layout.setContentsMargins(0, 0, 0, 0)
+        self.create_project_button = QPushButton(self.verticalLayoutWidget_2)
+        self.create_project_button.setObjectName(u"create_project_button")
+        self.create_project_button.setMouseTracking(False)
+        self.create_project_button.setContextMenuPolicy(Qt.ContextMenuPolicy.NoContextMenu)
+        icon = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::ListAdd"))
+        self.create_project_button.setIcon(icon)
+
+        self.project_tab_vertical_layout.addWidget(self.create_project_button, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
+
+        icon1 = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::DocumentOpen"))
+        self.toolBox.addItem(self.project_tab, icon1, u"Project")
+        self.annotate_tab = QWidget()
+        self.annotate_tab.setObjectName(u"annotate_tab")
+        self.annotate_tab.setGeometry(QRect(0, 0, 500, 334))
+        icon2 = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::MediaPlaybackStop"))
+        self.toolBox.addItem(self.annotate_tab, icon2, u"Create training data")
+        self.train_tab = QWidget()
+        self.train_tab.setObjectName(u"train_tab")
+        self.train_tab.setGeometry(QRect(0, 0, 500, 334))
+        icon3 = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::Computer"))
+        self.toolBox.addItem(self.train_tab, icon3, u"Train model")
+        self.predict_tab = QWidget()
+        self.predict_tab.setObjectName(u"predict_tab")
+        self.predict_tab.setGeometry(QRect(0, 0, 500, 334))
+        icon4 = QIcon(QIcon.fromTheme(u"QIcon::ThemeIcon::ViewFullscreen"))
+        self.toolBox.addItem(self.predict_tab, icon4, u"Predict on new videos")
+
+        self.mainLayout.addWidget(self.toolBox)
 
 
-        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.retranslateUi(octron_widgetui)
 
-        self.tabs.addTab(self.tab, "")
-        self.tab_2 = QWidget()
-        self.tab_2.setObjectName(u"tab_2")
-        self.tabs.addTab(self.tab_2, "")
-        self.tab_3 = QWidget()
-        self.tab_3.setObjectName(u"tab_3")
-        self.tabs.addTab(self.tab_3, "")
-        self.tab_4 = QWidget()
-        self.tab_4.setObjectName(u"tab_4")
-        self.tabs.addTab(self.tab_4, "")
-
-        self.retranslateUi(octron_gui)
-
-        self.tabs.setCurrentIndex(0)
+        self.toolBox.setCurrentIndex(0)
 
 
-        QMetaObject.connectSlotsByName(octron_gui)
+        QMetaObject.connectSlotsByName(octron_widgetui)
+    # setupUi
 
-    def retranslateUi(self, octron_gui):
-        octron_gui.setWindowTitle(QCoreApplication.translate("octron_gui", u"octron_gui", None))
-        self.pushButton.setText(QCoreApplication.translate("octron_gui", u"PushButton", None))
-        self.tabs.setTabText(self.tabs.indexOf(self.tab), QCoreApplication.translate("octron_gui", u"Project", None))
-        self.tabs.setTabText(self.tabs.indexOf(self.tab_2), QCoreApplication.translate("octron_gui", u"Segment", None))
-        self.tabs.setTabText(self.tabs.indexOf(self.tab_3), QCoreApplication.translate("octron_gui", u"Train", None))
-        self.tabs.setTabText(self.tabs.indexOf(self.tab_4), QCoreApplication.translate("octron_gui", u"Predict", None))
+    def retranslateUi(self, octron_widgetui):
+        octron_widgetui.setWindowTitle(QCoreApplication.translate("octron_widgetui", u"octron_gui", None))
+        self.octron_logo.setText("")
+        self.create_project_button.setText(QCoreApplication.translate("octron_widgetui", u"Create Project", None))
+        self.toolBox.setItemText(self.toolBox.indexOf(self.project_tab), QCoreApplication.translate("octron_widgetui", u"Project", None))
+#if QT_CONFIG(tooltip)
+        self.toolBox.setItemToolTip(self.toolBox.indexOf(self.project_tab), QCoreApplication.translate("octron_widgetui", u"Create new octron projects or load existing ones", None))
+#endif // QT_CONFIG(tooltip)
+        self.toolBox.setItemText(self.toolBox.indexOf(self.annotate_tab), QCoreApplication.translate("octron_widgetui", u"Create training data", None))
+#if QT_CONFIG(tooltip)
+        self.toolBox.setItemToolTip(self.toolBox.indexOf(self.annotate_tab), QCoreApplication.translate("octron_widgetui", u"Create segmentation data that can be used to train models ", None))
+#endif // QT_CONFIG(tooltip)
+        self.toolBox.setItemText(self.toolBox.indexOf(self.train_tab), QCoreApplication.translate("octron_widgetui", u"Train model", None))
+#if QT_CONFIG(tooltip)
+        self.toolBox.setItemToolTip(self.toolBox.indexOf(self.train_tab), QCoreApplication.translate("octron_widgetui", u"Train model with generated training data", None))
+#endif // QT_CONFIG(tooltip)
+        self.toolBox.setItemText(self.toolBox.indexOf(self.predict_tab), QCoreApplication.translate("octron_widgetui", u"Predict on new videos", None))
+#if QT_CONFIG(tooltip)
+        self.toolBox.setItemToolTip(self.toolBox.indexOf(self.predict_tab), QCoreApplication.translate("octron_widgetui", u"Use trained models to run predictions on new videos", None))
+#endif // QT_CONFIG(tooltip)
+    # retranslateUi
 
