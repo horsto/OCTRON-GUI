ff3f71ca082d3a9c3de9d4675329916f830989c9
Hotte <hobenhaus@gmail.com>
2025-04-02 19:45:08 -0400

    Update sam2_octron.py

--- octron/sam2_octron/helpers/sam2_octron.py
+++ octron/sam2_octron/helpers/sam2_octron.py
@@ -32,14 +32,13 @@ class SAM2_octron(SAM2VideoPredictor):
     ):
         
         # whether to apply non-overlapping constraints on the output object masks
-        non_overlap_masks=False,
+        non_overlap_masks=False
         # whether to clear non-conditioning memory of the surrounding frames (which may contain outdated information) after adding correction clicks;
         # note that this would only apply to *single-object tracking* unless `clear_non_cond_mem_for_multi_obj` is also set to True)
-        clear_non_cond_mem_around_input=True,
+        clear_non_cond_mem_around_input=True
         # if `add_all_frames_to_correct_as_cond` is True, we also append to the conditioning frame list any frame that receives a later correction click
         # if `add_all_frames_to_correct_as_cond` is False, we conditioning frame list to only use those initial conditioning frames
-        add_all_frames_to_correct_as_cond=True,
-        #self.fill_hole_area = fill_hole_area
+        add_all_frames_to_correct_as_cond=True
         self.non_overlap_masks = non_overlap_masks
         self.clear_non_cond_mem_around_input = clear_non_cond_mem_around_input
         self.add_all_frames_to_correct_as_cond = add_all_frames_to_correct_as_cond
@@ -121,7 +120,7 @@ class SAM2_octron(SAM2VideoPredictor):
         #self.fill_hole_area = 200
         # For morphological operations 
         self.disk_size = 2
-        self.perform_morphological_operations = False
+        self.perform_morphological_operations = True
         if self.perform_morphological_operations:
             self.closing_kernel = torch_tensor(disk(self.disk_size).tolist()).to(compute_device)
                     
