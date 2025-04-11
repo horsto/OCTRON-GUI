f163464a9b6fd8593df083a5c67a6f086ec5850b
Hotte <hobenhaus@gmail.com>
2025-04-11 19:34:02 -0400

    Fix a bug in polygon concatenation
    I am now using merge_multi_segment() from ultralytics to handle the concatenation of multiple polygons. See documentation under https://forum.image.sc/t/polygon-generation-artifacts/111267/2

--- octron/yolo_octron/helpers/polygons.py
+++ octron/yolo_octron/helpers/polygons.py
@@ -166,6 +166,77 @@ def watershed_mask(mask,
     return labels, masks
 
 
+def merge_multi_segment(segments):
+    """
+    This function is copied from ultralytics/utils/ops.py
+    
+    Merge multiple segments into one list by connecting the coordinates with the minimum distance between each segment.
+    This function connects these coordinates with a thin line to merge all segments into one.
+
+    Parameters
+    ----------
+    segments (List[np.ndarray]): A list of segments, where each segment is a NumPy array of shape (N, 2).
+                             
+    Returns
+    -------
+    s (List[np.ndarray]): A list of connected segments represented as NumPy arrays.
+
+    """
+    def _min_index(arr1, arr2):
+        """
+        Find a pair of indexes with the shortest distance between two arrays of 2D points.
+
+        Parameters
+        ----------
+        arr1 (np.ndarray): A NumPy array of shape (N, 2) representing N 2D points.
+        arr2 (np.ndarray): A NumPy array of shape (M, 2) representing M 2D points.
+        
+        Returns
+        -------
+        min_index1 (tuple): A tuple containing the indexes of the points with the 
+                            shortest distance in arr1 and arr2 respectively.
+        """
+        dis = ((arr1[:, None, :] - arr2[None, :, :]) ** 2).sum(-1)
+        return np.unravel_index(np.argmin(dis, axis=None), dis.shape)
+    
+    s = []
+    segments = [np.array(i).reshape(-1, 2) for i in segments]
+    idx_list = [[] for _ in range(len(segments))]
+
+    # Record the indexes with min distance between each segment
+    for i in range(1, len(segments)):
+        idx1, idx2 = _min_index(segments[i - 1], segments[i])
+        idx_list[i - 1].append(idx1)
+        idx_list[i].append(idx2)
+
+    # Use two round to connect all the segments
+    for k in range(2):
+        # Forward connection
+        if k == 0:
+            for i, idx in enumerate(idx_list):
+                # Middle segments have two indexes, reverse the index of middle segments
+                if len(idx) == 2 and idx[0] > idx[1]:
+                    idx = idx[::-1]
+                    segments[i] = segments[i][::-1, :]
+
+                segments[i] = np.roll(segments[i], -idx[0], axis=0)
+                segments[i] = np.concatenate([segments[i], segments[i][:1]])
+                # Deal with the first segment and the last one
+                if i in {0, len(idx_list) - 1}:
+                    s.append(segments[i])
+                else:
+                    idx = [0, idx[1] - idx[0]]
+                    s.append(segments[i][idx[0] : idx[1] + 1])
+
+        else:
+            for i in range(len(idx_list) - 1, -1, -1):
+                if i not in {0, len(idx_list) - 1}:
+                    idx = idx_list[i]
+                    nidx = abs(idx[1] - idx[0])
+                    s.append(segments[i][nidx:])
+    return s
+
+
 def get_polygons(mask):
     """
     Given a mask image, extract outlines as polygon 
@@ -192,14 +263,12 @@ def get_polygons(mask):
     assert set(np.unique(mask)) == set([1,0]), 'Image should be composed of 0s and 1s'   
     
     polygons = Mask(mask).polygons()
-
-    if len(polygons.points) > 1:
-        # Just fuse the lists
-        polygon_points = np.concatenate(polygons.points,axis=0)
-    else:
-        polygon_points = polygons.points[0]
-        
-    return polygon_points
+    polygons = (
+                    np.concatenate(merge_multi_segment([x.reshape(-1, 2) for x in polygons.points]))
+                    if len(polygons.points) > 1
+                    else polygons.points[0].reshape(-1, 2)
+                )
+    return polygons
 
 
 def polygon_to_mask(empty_mask, polygons, smooth_sigma=1.):
