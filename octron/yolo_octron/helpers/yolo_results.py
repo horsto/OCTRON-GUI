add44a8673aca93a312a0d4a9ce419dd679ec548
Hotte <hobenhaus@gmail.com>
2025-05-20 14:27:01 -0400

    Further work on results loader class

--- octron/yolo_octron/helpers/yolo_results.py
+++ octron/yolo_octron/helpers/yolo_results.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import zarr
+import numpy as np
 import pandas as pd
+import warnings
 # Plugins
 from napari_pyav._reader import FastVideoReader
 
@@ -16,12 +18,22 @@ class YOLO_results:
         
         
         """
+        # Ignore specific Zarr warning about .DS_Store
+        # That happens on Mac ... might include more here.
+        warnings.filterwarnings(
+            "ignore",
+            message="Object at .DS_Store is not recognized as a component of a Zarr hierarchy.",
+            category=UserWarning,
+            module="zarr.core.group"
+        )
+        
         # Process kwargs
         self.csv_header_lines = kwargs.get('csv_header_lines', 6)
         
         # Initialize some variables 
         self.verbose = verbose
         self.video, self.video_dict = None, None
+        self.width, self.height, self.num_frames = None, None, None 
         self.csvs = None
         self.zarr, self.zarr_root = None, None
         self.track_ids = None
@@ -49,6 +61,9 @@ class YOLO_results:
             if video_path.stem == '_'.join(results_dir.name.split('_')[:-1]):
                 video = FastVideoReader(video_path, read_format='rgb24')
                 video_dict = probe_video(video_path, verbose=self.verbose)
+                self.height = video_dict['height']
+                self.width = video_dict['width']
+                self.num_frames = video_dict['num_frames']
                 break
         if video is None and self.verbose:
             print(f"No video found for '{results_dir.name}'")
@@ -199,6 +214,58 @@ class YOLO_results:
         return self.track_ids, self.labels, self.track_id_label
     
     
+    def get_track_id_for_label(self, label):
+        """
+        Get the track ID for a given label.
+        
+        Parameters
+        ----------
+        label : str
+            The label to search for.
+            
+        Returns
+        -------
+        track_id : int
+            The track ID for the given label.
+            
+        """
+        if self.track_id_label is None:
+            raise ValueError("No track IDs found. Please run get_track_ids_labels() first.")
+        
+        track_id = self.find_class_by_name(self.track_id_label, label)
+        if track_id is None:
+            raise ValueError(f"Label '{label}' not found in track IDs.")
+        # TODO:
+        # Check what happens with multiple track IDs for the same label 
+        
+        return track_id
+    
+    
+    def get_label_for_track_id(self, track_id): 
+        """
+        Get the label for a given track ID.
+        
+        Parameters
+        ----------
+        track_id : int
+            The track ID to search for.
+            
+        Returns
+        -------
+        label : str
+            The label for the given track ID.
+            
+        """
+        if self.track_id_label is None:
+            raise ValueError("No track IDs found. Please run get_track_ids_labels() first.")
+        
+        label = self.track_id_label.get(track_id, None)
+        if label is None:
+            raise ValueError(f"Track ID '{track_id}' not found in labels.")
+        
+        return label
+    
+    
     #### CORE DATA EXTRACTION ##############################################################
     
     def get_tracking_data(self):
@@ -223,7 +290,7 @@ class YOLO_results:
                 track_id = df.iloc[0].track_id
                 label = df.iloc[0].label
                 if track_id not in tracking_data:
-                    tracking_data[track_id] = {
+                    tracking_data[int(track_id)] = {
                         'label': label,
                         'data': df
                     }
@@ -233,17 +300,125 @@ class YOLO_results:
                 if self.verbose:
                     print(f"Could not read tracking data from {csv_file.name}: {e}")
         return tracking_data
-        
-        
     
     
+    def get_tracking_for_label(self, label):
+        """
+        Get the tracking data for only one label.
+        
+        Parameters
+        ----------
+        label : str
+            The label to search for.
+            
+        Returns
+        -------
+        tracking_data : pandas.DataFrame
+            The tracking data for the given label.
+        frame_indices : numpy.ndarray
+            The frame indices for which tracking data exist.
+        """
+        if self.track_id_label is None:
+            raise ValueError("No track IDs found. Please run get_track_ids_labels() first.")
+        track_id = self.get_track_id_for_label(label)
+        tracking_data = self.get_tracking_data()
+        if track_id not in tracking_data:
+            raise ValueError(f"Track ID '{track_id}' not found in tracking data.")
+        
+        # Find out which indices have data
+        frame_indices =  tracking_data[track_id]['data']['frame_idx'].values
+        return tracking_data[track_id]['data'], frame_indices
+    
+    def get_mask_data(self):
+        """
+        Get the mask data for all track IDs in a dictionary
+        of track_id -> mask data
+        
+        Returns
+        -------
+        mask_data : dict
+            Dictionary of track_id -> mask data
+            Where mask data is a numpy array with the mask data.
+            
+        """
+        if self.zarr_root is None:
+            raise ValueError("No zarr root found, cannot extract mask data.")
+        assert self.track_ids is not None, "No track IDs found. Please run get_track_ids_labels() first."
+        assert self.track_id_label is not None, "No track ID labels found. Please run get_track_ids_labels() first."
+        
+        mask_data = {}
+        for track_id in self.track_ids:
+            label = self.get_label_for_track_id(track_id)
+            try:
+                mask_key = f"{track_id}_masks"
+                if mask_key in self.zarr_root.array_keys():
+                    masks = self.zarr_root[mask_key]
+                    num_frames = masks.shape[0]
+                    height = masks.shape[1]
+                    width = masks.shape[2]
+                    if self.num_frames is not None: 
+                        assert num_frames == self.num_frames, \
+                            f"Number of frames in mask data ({num_frames}) does not match number of frames in video ({self.num_frames})."
+                    if self.height is not None:
+                        assert height == self.height, \
+                            f"Height in mask data ({height}) does not match height in video ({self.height})."
+                    if self.width is not None:
+                        assert width == self.width, \
+                            f"Width in mask data ({width}) does not match width in video ({self.width})."
+                    mask_data[track_id] = {
+                        'label': label,
+                        'data' : masks,
+                    }
+                else:
+                    if self.verbose:
+                        print(f"Mask key '{mask_key}' not found in zarr archive.")  
+            except Exception as e:
+                if self.verbose:
+                    print(f"Could not read mask data for track ID {track_id}: {e}")
+        return mask_data
+    
+    def get_masks_for_label(self, label):
+        """
+        Get the mask data for only one label.
+        
+        Parameters
+        ----------
+        label : str
+            The label to search for.
+            
+        Returns
+        -------
+        mask_data : zarr.Array
+            The mask data for the given label.
+        frame_indices : numpy.ndarray
+            The frame indices for which data exist in the mask data.
+        """
+        if self.track_id_label is None:
+            raise ValueError("No track IDs found. Please run get_track_ids_labels() first.")
+        track_id = self.get_track_id_for_label(label)
+        mask_data = self.get_mask_data()
+        if track_id not in mask_data:
+            raise ValueError(f"Track ID '{track_id}' not found in mask data.")
+        
+        # Find out which indices have data
+        frame_indices = np.where(
+            (mask_data[track_id]['data'][:,0,0] != -1)
+        )[0]
+        if len(frame_indices) == 0:
+            raise ValueError(f"No valid frames found for track ID '{track_id}' in mask data.")
+        return mask_data[track_id]['data'], frame_indices
     
     
     def __repr__(self) -> str:
-        return f"YOLO_results ({self.results_dir})"
+        if self.num_frames is not None and self.width is not None and self.height is not None:
+            return f"YOLO_results\n{self.results_dir}\n{self.num_frames} frames, {self.width}x{self.height}"
+        else:
+            return f"YOLO_results\n{self.results_dir}"
     def __str__(self) -> str:
-        return f"YOLO_results ({self.results_dir})"
-    
+        if self.num_frames is not None and self.width is not None and self.height is not None:
+            return f"YOLO_results\n{self.results_dir}\n{self.num_frames} frames, {self.width}x{self.height}"
+        else:
+            return f"YOLO_results\n{self.results_dir}"
     
     #### OTHER HELPERS #######################################################################
     
