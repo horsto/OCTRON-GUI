69649ed4d3c6395edc7a446c7ece9acd0d123869
Hotte <hobenhaus@gmail.com>
2025-05-25 16:11:10 -0400

    Bump version

--- octron/__init__.py
+++ octron/__init__.py
@@ -1,13 +1,18 @@
-from ._version import version as __version__
+import importlib.metadata
+from importlib.metadata import version
+
+try:
+    __version__ = version("octron")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "no version"  
+
 from .main import octron_widget
 from .reader import octron_reader
-
 from .yolo_octron.helpers.yolo_results import YOLO_results
 
 __all__ = (
     "octron_widget",
     "octron_reader",
     "YOLO_results",
-    
-    )
+)
 
